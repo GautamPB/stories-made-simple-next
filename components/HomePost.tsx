be785f071300639cab3182214e29be8edd7d3816
Gautam <pbg2402@gmail.com>
2022-02-05 01:39:11 +0530

    Added: description field

--- components/HomePost.tsx
+++ components/HomePost.tsx
@@ -10,13 +10,12 @@ export default function HomePost({ post }: Props) {
         <div className="group my-2 mr-5 flex cursor-pointer flex-col items-start justify-center overflow-hidden rounded-lg transition hover:bg-[#e7e7e7]">
             <div className="group w-full overflow-hidden rounded-lg border">
                 <img
-                    // src="https://www.wallpapers13.com/wp-content/uploads/2015/11/Tower-for-surveillance-of-forest-fire-night-nature-landscape-Wallpaper-HD-1920x1080.jpg"
                     src={urlFor(post.mainImage).url()!}
                     className="group h-60 w-full transform object-cover transition duration-200 ease-in-out group-hover:scale-105"
                 />
             </div>
 
-            <div className="p-2">
+            <div className="space-y-1 p-2">
                 <h1 className="text-lg font-bold">{post.title}</h1>
                 <h3>
                     <span className="text-[#0EBFE9] group-hover:font-bold">
@@ -27,6 +26,8 @@ export default function HomePost({ post }: Props) {
                         &nbsp;{new Date(post._createdAt)?.toDateString()}
                     </span>
                 </h3>
+
+                <p>{post.description}</p>
             </div>
         </div>
     )
